e6610059c7ccc317f8554ea40523ca907506b4a8
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2021-06-14 08:47:06 +0200

    jwt loginout

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 from app.config import Config
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=Config.PORT, debug=True)
+    app.run(host="127.0.0.1", port=Config.PORT, debug=True)
