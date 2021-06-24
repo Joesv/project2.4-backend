7ef7d0f58448aff86a08d95b2667134c60bc09f1
Remerman <56107094+Remerman@users.noreply.github.com>
2021-06-24 17:22:12 +0200

    fix merch conflicts

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,11 @@
 from app.routes.user import user
 from app.routes.lamp_device import lamp_device
 from app.routes.device import device
+from app.routes.dummy_device import dummy_device
 
 
 def register_blueprints(app):
     app.register_blueprint(user)
     app.register_blueprint(lamp_device)
     app.register_blueprint(device)
+    app.register_blueprint(dummy_device)
